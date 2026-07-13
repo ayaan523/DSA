6ff464b88c0117b0425fdfa6fe4e5b795ec333ad
Ayaan Ahamed F <160225643+ayaan523@users.noreply.github.com>
2026-07-13 10:43:14 +0530

    Time: 2 ms (89.04%) | Memory: 48 MB (43.16%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        
+        int maxDist=0;
+        for(int i=0;i<nums.length;i++){
+            if(i>maxDist){
+                return false;
+            }
+            maxDist=Math.max(maxDist, i+nums[i]);
+        }
+        return true;
+    }
+}
