f048baa8025cf29f3a1333db27ddf6ed107fa14f
Ayaan Ahamed F <160225643+ayaan523@users.noreply.github.com>
2026-07-23 18:23:57 +0530

    Time: 15 ms (23.72%) | Memory: 46.5 MB (34.43%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> S = new HashMap<>();
+        HashMap<Character, Integer> T = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            S.put(ch, S.getOrDefault(ch, 0) + 1);
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+
+            T.put(ch, T.getOrDefault(ch, 0) + 1);
+        }
+
+        return S.equals(T);
+    }
+}
