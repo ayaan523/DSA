ded1d6b389f618e21ad910a498913717ed668eb4
Ayaan Ahamed F <160225643+ayaan523@users.noreply.github.com>
2026-07-26 23:25:43 +0530

    Time: 35 ms (35.98%) | Memory: 100 MB (39.10%) - LeetSync

---
+++ 155-min-stack/min-stack.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+class MinStack {
+
+    Stack<Integer> stack;
+    Stack<Integer> minstack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minstack = new Stack<>();
+    }
+    
+    public void push(int value) {
+        stack.push(value);
+
+        if (minstack.isEmpty()) {
+            minstack.push(value);
+        } else {
+            minstack.push(Math.min(value, minstack.peek()));
+        }
+    }
+    
+    public void pop() {
+        stack.pop();
+        minstack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minstack.peek();
+    }
+}
