3cd48ad262d0e6f0d2ab35a52fc5317eb2a78aa5
Ayaan Ahamed F <160225643+ayaan523@users.noreply.github.com>
2026-08-01 17:27:45 +0530

    Time: 18 ms (60.57%) | Memory: 108.1 MB (29.16%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -5,10 +5,10 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             if(set.contains(nums[i])){
                 return true;
-            }else{
-                set.add(nums[i]);
-
             }
-        }return false;
+            set.add(nums[i]);
+            
+        }
+        return false;
     }
 }
